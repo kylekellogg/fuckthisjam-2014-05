2ccb42ded7acc2ce8379ba17e0da40c75681ab18
Kyle Kellogg <kyle@kylekellogg.com>
2014-05-31 14:31:18 -0400

    Added to DisplayObject base class

--- farmzilla/Assets/Scripts/DisplayObject.cs
+++ farmzilla/Assets/Scripts/DisplayObject.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
+[RequireComponent (typeof (SpriteRenderer))]
 public class DisplayObject : MonoBehaviour {
   protected SpriteRenderer _spriteRenderer;
   public SpriteRenderer spriteRenderer {
