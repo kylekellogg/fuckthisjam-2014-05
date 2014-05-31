f92fb94039fb19b831c1b52219e2d485b0cc8f18
Kyle Kellogg <kyle@kylekellogg.com>
2014-05-31 18:14:35 -0400

    Simple state machine and main controller

---
+++ farmzilla/Assets/Scripts/Controller/MainController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Gamestate : uint {
+  Title,
+  HUD,
+  GameOver
+}
+
+public class MainController : MonoBehaviour {
+  public GameObject[] GameStates;
+
+  protected GameObject CurrentGameState;
+
+  public void Awake() {
+    SwitchState( Gamestate.Title );
+  }
+
+  public void SwitchState( Gamestate state ) {
+    for ( int i = 0, l = GameStates.Length; i < l; i++ ) {
+      Debug.Log( "Will set " + i + " to be active? " + (i == (int)state) );
+      if ( GameStates[i] != null ) {
+        GameStates[i].SetActive( i == (int)state );
+      }
+    }
+  }
+}
