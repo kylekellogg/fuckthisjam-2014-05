54b5a0d32733776ee50790937ae495840afd7c86
Dan Barron <stereoalliance@gmail.com>
2014-05-31 22:38:50 -0400

    play button changes to HUD state, update gitignore, add play label

--- farmzilla/Assets/Scripts/Controller/TitleController.cs
+++ farmzilla/Assets/Scripts/Controller/TitleController.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class TitleController : BaseController {
 
+	public Button PlayButton;
+
 	// Use this for initialization
 	void Start () {
-	
+		PlayButton.MouseDown += HandleMouseDown;
+	}
+
+	void HandleMouseDown (DisplayObject displayObject)
+	{
+		MainController.SwitchState(Gamestate.HUD);
 	}
 	
 	// Update is called once per frame
