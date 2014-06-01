a88a917a9be68510713cab232cf933e1673b19e6
Kyle Kellogg <kyle@kylekellogg.com>
2014-06-01 13:32:56 -0400

    Updates to Button and HUDComponent to align their GUI.Label's appropriately

--- farmzilla/Assets/Scripts/UI/Button.cs
+++ farmzilla/Assets/Scripts/UI/Button.cs
@@ -69,7 +69,24 @@ public class Button : DisplayObject
 	public void OnGUI () {
 		Vector3 min = Camera.main.WorldToScreenPoint( _spriteRenderer.bounds.min );
 		Vector3 max = Camera.main.WorldToScreenPoint( _spriteRenderer.bounds.max );
-		Rect rect = new Rect( min.x, min.y, max.x - min.x, max.y - min.y );
+
+		Vector2 gmin = GUIUtility.ScreenToGUIPoint( new Vector2( min.x, Screen.height - min.y ) );
+		Vector2 gmax = GUIUtility.ScreenToGUIPoint( new Vector2( max.x, Screen.height - max.y ) );
+
+		/**
+		* KK, 2014-06-01
+		* 
+		* Unity's coordinate system (-1, -1) as Top Left and (1, 1) as Top Right
+		* mean that we must enter the Rect coordinates in a seemingly odd way
+		* which is x = least x (min, therefore left)
+		* and y = greatest y (max, therefore top)
+		* The differences are likewise reversed w/ x being greatest - least
+		* and y being least - greatest
+		* 
+		* The Screen.height - y coords above fix an offset issue that I'm not 100%
+		* certain about. As it's working, please leave it.
+		*/
+		Rect rect = new Rect( gmin.x, gmax.y, gmax.x - gmin.x, gmin.y - gmax.y );
 		GUI.Label( rect, Label, style );
 	}
 
--- farmzilla/Assets/Scripts/UI/HUDComponent.cs
+++ farmzilla/Assets/Scripts/UI/HUDComponent.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (BoxCollider2D))]
 public class HUDComponent : DisplayObject {
 
 	public MainController MainController;
@@ -21,19 +20,26 @@ public class HUDComponent : DisplayObject {
 	}
 
 	public void OnGUI () {
-		Vector3 rawMin = _spriteRenderer.bounds.min;
-		Vector3 rawMax = _spriteRenderer.bounds.max;
+		Vector3 min = Camera.main.WorldToScreenPoint( _spriteRenderer.bounds.min );
+		Vector3 max = Camera.main.WorldToScreenPoint( _spriteRenderer.bounds.max );
 
-		rawMin.y *= -1;
-		rawMax.y *= -1;
+		Vector2 gmin = GUIUtility.ScreenToGUIPoint( new Vector2( min.x, Screen.height - min.y ) );
+		Vector2 gmax = GUIUtility.ScreenToGUIPoint( new Vector2( max.x, Screen.height - max.y ) );
 
-		Vector3 min = Camera.main.WorldToScreenPoint( rawMin );
-		Vector3 max = Camera.main.WorldToScreenPoint( rawMax );
-
-		Debug.Log ("Min: " + min.ToString());
-		Debug.Log ("Max: " + max.ToString());
-
-		Rect rect = new Rect( min.x, min.y, max.x - min.x, max.y - min.y );
+		/**
+		* KK, 2014-06-01
+		* 
+		* Unity's coordinate system (-1, -1) as Top Left and (1, 1) as Top Right
+		* mean that we must enter the Rect coordinates in a seemingly odd way
+		* which is x = least x (min, therefore left)
+		* and y = greatest y (max, therefore top)
+		* The differences are likewise reversed w/ x being greatest - least
+		* and y being least - greatest
+		* 
+		* The Screen.height - y coords above fix an offset issue that I'm not 100%
+		* certain about. As it's working, please leave it.
+		*/
+		Rect rect = new Rect( gmin.x, gmax.y, gmax.x - gmin.x, gmin.y - gmax.y );
 		GUI.Label( rect, Label, style );
 	}
 
@@ -42,7 +48,7 @@ public class HUDComponent : DisplayObject {
 		base.Initialize();
 
 		style = new GUIStyle();
-		style.alignment = TextAnchor.MiddleCenter;
+		style.alignment = TextAnchor.UpperLeft;
 		style.richText = true;
 
 		Label = "<color=#ffffff>" + MainController.Money + "</color>";
