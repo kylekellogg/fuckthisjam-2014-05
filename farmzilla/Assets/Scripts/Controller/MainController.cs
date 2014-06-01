88555e6c14c731bd7a8bf9becaa1d958e24b0ad7
Kyle Kellogg <kyle@kylekellogg.com>
2014-05-31 20:37:18 -0400

    Time conversion and money earning per time period

--- farmzilla/Assets/Scripts/Controller/MainController.cs
+++ farmzilla/Assets/Scripts/Controller/MainController.cs
@@ -12,10 +12,18 @@ public enum Gamestate : uint {
 public class MainController : MonoBehaviour {
   public GameObject[] GameStates;
 
+  public float IncomePerTimePeriod = 10.0f;
+  public float IncomeTimePeriodInDays = 0.25f;
+
   protected GameObject CurrentGameState;
 
   public void Awake() {
     SwitchState( Gamestate.Title );
+
+    LogTime( "yesterday", TimeConverter.GameTimeSince( DateTime.Now.AddDays( -1.0 ) ) );
+    LogTime( "two days ago", TimeConverter.GameTimeSince( DateTime.Now.AddDays( -2.0 ) ) );
+    LogTime( "last week", TimeConverter.GameTimeSince( DateTime.Now.AddDays( -7.0 ) ) );
+    LogTime( "one hour ago", TimeConverter.GameTimeSince( DateTime.Now.AddHours( -1.0 ) ) );
   }
 
   public void SwitchState( Gamestate state ) {
@@ -25,5 +33,14 @@ public class MainController : MonoBehaviour {
         GameStates[i].SetActive( i == (int)state );
       }
     }
+  }
+
+  public float MoneyEarnedForTimeSpan( TimeSpan timespan ) {
+    return (float)(timespan.TotalDays / IncomeTimePeriodInDays) * IncomePerTimePeriod;
+  }
+
+  private void LogTime( string since, TimeSpan timespan ) {
+    Debug.Log( "Game time since " + since + ": " + timespan.Days + " days, " + timespan.Hours + " hours, " + timespan.Minutes + " minutes, and " + timespan.Seconds + " seconds" );
+    Debug.Log( "Earned: " + MoneyEarnedForTimeSpan( timespan ) );
   }
 }
---
+++ farmzilla/Assets/Scripts/Utils/TimeConverter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class TimeConverter {
+  //  8 3-hour days per 24 hour period
+  public const float DAY_CONVERSION_RATE = 8.0f;
+
+  public static TimeSpan GameTimeSince( DateTime date ) {
+    DateTime now = DateTime.Now;
+    TimeSpan diff = now.Subtract( date );
+    double totalDays = diff.TotalDays * DAY_CONVERSION_RATE;
+    Debug.Log( "Time Conversion: " + diff.TotalDays.ToString() + " will become " + totalDays.ToString() + " in-game days" );
+    return TimeSpan.FromDays( totalDays );
+  }
+}
