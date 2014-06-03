e0e6925204b4e0bd1b38b6fa810c4437a01ab699
Dan Barron <stereoalliance@gmail.com>
2014-06-02 21:23:48 -0400

    first pass at job queue

---
+++ farmzilla/Assets/Scripts/Jobs/Job.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Job : MonoBehaviour {
+
+    public TimeSpan CreatedAt;
+    public TimeSpan WillBeCompletedOn;
+    public DisplayObject plot;
+
+    protected float progress;
+    protected bool isCompleted;
+    protected bool isActive;
+
+    void Schedule (DisplayObject pl)
+    {
+        isActive = false;
+        isCompleted = false;
+        plot = pl;
+        progress = 0f;
+        CreatedAt = TimeConverter.GameTimeSince( DateTime.Now );
+        WillBeCompletedOn = TimeConverter.GameTimeSince( DateTime.Now.AddHours(16) );
+    }
+
+    public void Activate ()
+    {
+        isActive = true;
+    }
+
+    public void Complete ()
+    {
+        isCompleted = true;
+    }
+
+    public void Tick (TimeSpan gameTime)
+    {
+        TimeSpan startOffset = gameTime - CreatedAt;
+        TimeSpan endOffset = WillBeCompletedOn - CreatedAt;
+
+        progress = (float) (startOffset - endOffset).TotalHours;
+    }
+}
---
+++ farmzilla/Assets/Scripts/Jobs/JobQueue.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JobQueue : MonoBehaviour {
+
+    protected List<Job> jobs;
+
+    public JobQueue ()
+    {
+        jobs = new List<Job> ();
+    }
+
+    public void Enqueue (Job job)
+    {
+        jobs.Add (job);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        TimeSpan now = TimeConverter.GameTimeSince( DateTime.Now );
+
+	    foreach (Job job in jobs) {
+            TimeSpan duration = job.WillBeCompletedOn.Subtract( now ).Duration();
+
+            if (duration <= TimeSpan.Zero) {
+                job.Complete();
+                jobs.Remove(job);
+            } else {
+                job.Tick (now);
+            }
+        }
+	}
+}
